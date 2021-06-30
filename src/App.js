8d451ef57bf43ba787482ff0ea92d91378090489
slavaMalenko <denouement20@yandex.ru>
2021-06-30 21:21:48 +0300

    correction of imperfections of the layout transfer

--- src/App.js
+++ src/App.js
@@ -11,84 +11,83 @@ import parrot from './assets/img/top/parrot.png';
 function App() {
   return (
     <div>
-      <header class="header">
-        <div class="container">
-          <div class="header__content">
-            <a class="logo" href="#">
-              <div class="logo__img">
-                <img class="logo__img-photo" src={logo} alt="" />
+
+      <header className="header">
+        <div className="container">
+          <div className="header__content">
+            <a className="logo" href="#">
+              <div className="logo__img">
+                <img className="logo__img-photo" src={logo} alt="" />
               </div>
-              <span class="logo__text">HAPPY PET</span>
+              <span className="logo__text">HAPPY PET</span>
             </a>
 
-            <div class="menu">
-              <ul class="menu__items">
-                <li class="menu__item">
-                  <a class="menu__link menu__link--active" href="#">Главная</a>
+            <div className="menu">
+              <ul className="menu__items">
+                <li className="menu__item">
+                  <a className="menu__link menu__link--active" href="#">Главная</a>
                 </li>
 
-                <li class="menu__item">
-                  <a class="menu__link" href="#">Друзья</a>
+                <li className="menu__item">
+                  <a className="menu__link" href="#">Друзья</a>
                 </li>
 
-                <li class="menu__item">
-                  <a class="menu__link" href="#">О сервисе</a>
+                <li className="menu__item">
+                  <a className="menu__link" href="#">О сервисе</a>
                 </li>
 
-                <li class="menu__item">
-                  <a class="menu__link" href="#">Контакты</a>
+                <li className="menu__item">
+                  <a className="menu__link" href="#">Контакты</a>
                 </li>
               </ul>
             </div>
 
-            <a class="tel" href="#">
-              <img class="tel__img" src={tel} alt="" />
-              <span class="tel__cont">+7 978 884 43-78</span>
+            <a className="tel" href="#">
+              <img className="tel__img" src={tel} alt="" />
+              <span className="tel__cont">+7 978 884 43-78</span>
             </a>
 
-            <div class="basket">
-              <a class="basket__link" href="#">
-                <img class=" basket__link-img" src={basket} alt="" />
+            <div className="basket">
+              <a className="basket__link" href="#">
+                <img className=" basket__link-img" src={basket} alt="" />
               </a>
 
-              <a class="basket__list" href="#">
-                <span class="basket__list-price">10 000 ₽</span>
+              <a className="basket__list" href="#">
+                <span className="basket__list-price">10 000 ₽</span>
 
-                <img class="basket__list-arrow" src={arrow} alt="" />
+                <img className="basket__list-arrow" src={arrow} alt="" />
               </a>
             </div>
           </div>
         </div>
       </header>
 
-      <main class="main">
-        <div class="container">
-          <section class="base">
-            <img class="intro" src={animals} alt="" />
+      <main className="main">
+        <div className="container">
+          <section className="base">
+            <img className="intro" src={animals} alt="" />
 
-            <div class="description">
-              <h1 class="description__head">Твой новый друг <br /> с доставкой на дом</h1>
-              <p class="description__item">Онлайн-магазин породистых домашних животных подарит тебе нового друга в
-                несколько
-                кликов. Действуй!</p>
-              <a href="" class="description__button">Посмотреть друзей</a>
+            <div className="description">
+              <h1 className="description__head">Твой новый друг <br /> с доставкой на дом</h1>
+              <p className="description__item">Онлайн-магазин породистых домашних животных подарит тебе нового друга в несколько кликов. Действуй!</p>
+              <a href="" className="description__button">Посмотреть друзей</a>
             </div>
 
-            <div class="top">
-              <h3 class="top__head">Топ месяца:</h3>
+            <div className="top">
+              <h3 className="top__head">Топ месяца:</h3>
 
-              <ul class="top__items">
-                <li class="top__item">
-                  <a class="top__item-link" href="#">
-                    <span class="top__item-head">Собаки</span>
-                    <img class="top__item-img top__item-img--center" src={dog} alt="" />
+              <ul className="top__items">
+                <li className="top__item">
+                  <a className="top__item-link" href="#">
+                    <span className="top__item-head">Собаки</span>
+                    <img className="top__item-img top__item-img--center" src={dog} alt="" />
                   </a>
                 </li>
 
-                <li class="top__item">
-                  <a class="top__item-link" href="#">
-                    <span class="top__item-head top__item-head--arrow">Попугаи</span>
-                    <img class="top__item-img" src={parrot} alt="" />
+                <li className="top__item">
+                  <a className="top__item-link" href="#">
+                    <span className="top__item-head top__item-head--arrow">Попугаи</span>
+                    <img className="top__item-img" src={parrot} alt="" />
                   </a>
                 </li>
               </ul>
@@ -96,6 +95,7 @@ function App() {
           </section>
         </div>
       </main>
+
     </div>
   );
 }
