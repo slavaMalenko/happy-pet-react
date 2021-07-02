69ce5854264a513c9e3ed9e881e939de18abf6c2
slavaMalenko <denouement20@yandex.ru>
2021-07-03 01:05:06 +0300

    work with store filters

---
+++ src/redux/reducers/filters.js
@@ -0,0 +1,19 @@
+const initialState = {
+    sortBy: 'price',
+    category: null,
+}
+
+const filters = (state = initialState, action) => {
+    switch (action.type) {
+        case 'SET_CATEGORY':
+            return {
+                ...state,
+                category: action.payload
+            }
+
+        default:
+            return state;
+    }
+}
+
+export default filters;
