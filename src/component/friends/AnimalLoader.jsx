7244ba6f434f6140d352f5d3068513eca12408c8
slavaMalenko <denouement20@yandex.ru>
2021-07-04 01:10:26 +0300

    changing the loading of animals

--- src/component/friends/AnimalLoader.jsx
+++ src/component/friends/AnimalLoader.jsx
@@ -7,14 +7,14 @@ const AnimalLoader = (props) => (
         width={300}
         height={379}
         viewBox="0 0 300 379"
-        backgroundColor="#f3f3f3"
-        foregroundColor="#ecebeb"
+        backgroundColor="#ffc9796a"
+        foregroundColor="#ffd6986a"
         {...props}
     >
         <rect x="7" y="0" rx="10" ry="10" width="270" height="251" />
-        <rect x="98" y="255" rx="0" ry="0" width="80" height="25" />
-        <rect x="62" y="287" rx="0" ry="0" width="156" height="18" />
-        <rect x="7" y="310" rx="78" ry="78" width="270" height="68" />
+        <rect x="98" y="255" rx="8" ry="8" width="80" height="20" />
+        <rect x="63" y="280" rx="17" ry="17" width="156" height="18" />
+        <rect x="7" y="304" rx="40" ry="40" width="270" height="68" />
     </ContentLoader>
 )
 
