724ce856bcce84b7e0ea0e5c80677593082e88f4
slavaMalenko <denouement20@yandex.ru>
2021-07-05 16:25:37 +0300

    redrawing the animal layout

--- src/component/friends/Animals.jsx
+++ src/component/friends/Animals.jsx
@@ -8,22 +8,27 @@ function Animals({ items, changeCartPrice }) {
                 items.map((item, index) => {
                     return (
                         <div className="animal">
-                            <img className="animal__img" src={item.imageUrl} alt="фвыфвы" />
+                            <div className="animal-bcg">
+                                <img className="animal__img" src={item.imageUrl} alt="" />
 
-                            <div className="animal__info">
-                                <h3 className="animal__info-name">{item.name}</h3>
-                                <span className="animal__info-breed">{item.kind}, </span>
-                                <span className="animal__info-age">{item.age} {item.ageValue}</span>
-                            </div>
+                                <div className="animal__info">
+                                    <h3 className="animal__info-name">{item.name}</h3>
+                                    <p className="animal__info-description">{item.description}</p>
+                                </div>
 
-                            <div className="animal__price">
-                                <button
-                                    className="animal__price-button"
-                                    onClick={() => changeCartPrice(item.price)}
-                                >
-                                    Подружиться
-                                </button>
-                                <p className="animal__price-value">{item.price} ₽</p>
+                                <div className="animal__bottom">
+                                    <span className="animal__bottom-breed">{item.kind}, </span>
+                                    <span className="animal__bottom-age">{item.age} {item.ageValue}</span>
+                                    <div className="animal__price">
+                                        <button
+                                            className="animal__price-button"
+                                            onClick={() => changeCartPrice(item.price)}
+                                        >
+                                            Подружиться
+                                        </button>
+                                        <p className="animal__price-value">{item.price} ₽</p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     )
