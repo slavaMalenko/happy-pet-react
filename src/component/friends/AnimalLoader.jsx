c672b3b8a349b26a4c4400583396f76c993b72c3
slavaMalenko <denouement20@yandex.ru>
2021-07-05 03:07:22 +0300

    layout fixes

--- src/component/friends/AnimalLoader.jsx
+++ src/component/friends/AnimalLoader.jsx
@@ -4,11 +4,11 @@ import ContentLoader from "react-content-loader";
 const AnimalLoader = (props) => (
     <ContentLoader
         speed={2}
-        width={300}
+        width={290}
         height={379}
         viewBox="0 0 300 379"
-        backgroundColor="#ffc9796a"
-        foregroundColor="#ffd6986a"
+        backgroundColor="#eeeeee6a"
+        foregroundColor="#d3d3d36a"
         {...props}
     >
         <rect x="7" y="0" rx="10" ry="10" width="270" height="251" />
