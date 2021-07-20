8a8fa8b5221fe0c583ba201437ebfcb3a6d291af
slavaMalenko <denouement20@yandex.ru>
2021-07-20 22:56:32 +0300

    component creation about us

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import { Route } from 'react-router-dom';
 
 import Header from './component/header/header';
+import AboutUs from './pages/AboutUs';
 import BaseHome from './pages/BaseHome';
 import Contacts from './pages/Contacts';
 import Friends from './pages/Friends';
@@ -24,6 +25,7 @@ function App() {
         <div className="container">
           <Route path="/" exact component={BaseHome} />
           <Route path="/friends" exact component={Friends} />
+          <Route path="/service" exact component={AboutUs} />
           <Route path="/contacts" exact component={Contacts} />
         </div>
       </main>
